d53f99306c8609aa6b3622073092c6bcadfda105
Heikki <heikki.pulli@ad.helsinki.fi>
2019-11-17 16:29:56 +0200

    gradle versio muutettu

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -1,16 +1,16 @@
 package ohtu.ohtuvarasto;
 
-import org.junit.*;
+//import org.junit.*;
 import static org.junit.Assert.*;
 
-import javax.sound.sampled.SourceDataLine;
+//import javax.sound.sampled.SourceDataLine;
 
-import org.junit.After;
-import org.junit.AfterClass;
+//import org.junit.After;
+//import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
+//import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
+//import static org.junit.Assert.*;
 
 public class VarastoTest {
 
